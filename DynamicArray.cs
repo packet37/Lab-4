97634eb59b24a0e951e18cd340a6e0618bdc66fd
packet37 <125910700+packet37@users.noreply.github.com>
2023-05-23 19:27:58 +0300

    Add files via upload

---
+++ DynamicArray.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    class DynamicArray
+    {
+        private int[] array;    // Массив для хранения элементовв
+        private int size;       // Текущее количество элементов в массиве
+        private int capacity;   // Максимальное количество элементов, которое может храниться в массиве
+
+        // Конструктор: для создания нового динамического массивв указанной емкости
+        public DynamicArray(int capacity)
+        {
+            this.array = new int[capacity];
+            this.size = 0;
+            this.capacity = capacity;
+        }
+
+        // Метод автоматического заполнения массива в пределах заданного числового диапазона
+        public void Populate(int start, int end)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                Add(i);
+            }
+        }
+
+        // Метод для чтения элемента по индексу
+        public int Get(int index)
+        {
+            if (index < 0 || index >= size)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            return array[index];
+        }
+
+        // Метод для записи элемента по индексу
+        public void Set(int index, int value)
+        {
+            if (index < 0 || index >= size)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            array[index] = value;
+        }
+
+        // Метод для добавления элемента в конец массива
+        public void Add(int element)
+        {
+            // Проверяем, заполнен ли массив, если да, увеличить емкость
+            if (size == capacity)
+            {
+                int newCapacity = capacity * 2;
+                int[] newArray = new int[newCapacity];
+                for (int i = 0; i < size; i++)
+                {
+                    newArray[i] = array[i];
+                }
+                array = newArray;
+                capacity = newCapacity;
+            }
+            array[size] = element;
+            size++;
+        }
+
+        // Метод для удаления элемента с конца массива
+        public void Remove()
+        {
+            if (size == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            size--;
+        }
+
+        // Метод для вставки элемента по индексу
+        public void Insert(int index, int element)
+        {
+            if (index < 0 || index > size)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            // Проверяем, заполнен ли массив, если да, увеличить емкость
+            if (size == capacity)
+            {
+                int newCapacity = capacity * 2;
+                int[] newArray = new int[newCapacity];
+                for (int i = 0; i < size; i++)
+                {
+                    newArray[i] = array[i];
+                }
+                array = newArray;
+                capacity = newCapacity;
+            }
+            // Сдвигаем элементы вправо, чтобы освободить место для нового элемента
+            for (int i = size - 1; i >= index; i--)
+            {
+                array[i + 1] = array[i];
+            }
+            array[index] = element;
+            size++;
+        }
+
+        // Метод для удаления элемента по индексу
+        public void Delete(int index)
+        {
+            if (index < 0 || index >= size)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            // Сдвигаем элементы влево, чтобы заполнить пробел
+            for (int i = index; i < size - 1; i++)
+            {
+                array[i] = array[i + 1];
+            }
+            size--;
+        }
+        // Метод для удаления элемента по значению
+        public void DeleteValue(int value)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                if (array[i] == value)
+                {
+                    Delete(i);
+                }
+            }
+        }
+        // Метод для возврата максимального значения и его индекса в массиве
+        public int[] Max()
+        {
+            if (size == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            int maxIndex = 0;
+            for (int i = 1; i < size; i++)
+            {
+                if (array[i] > array[maxIndex])
+                {
+                    maxIndex = i;
+                }
+            }
+            return new int[] { maxIndex, array[maxIndex] };
+        }
+
+        // Метод для проверки на переполнение
+        public bool IsFull()
+        {
+            return size == capacity;
+        }
+
+        // Метод для возврата фактического количества элементов в массиве
+        public int Count()
+        {
+            return size;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите ёмкость массива:");
+            int capacity = int.Parse(Console.ReadLine());
+            DynamicArray array = new DynamicArray(capacity); array.Populate(1, 5);
+            Console.WriteLine("Массив после заполнения значениями от 1 до 5:");
+            for (int i = 0; i < array.Count(); i++)
+            {
+                Console.Write(array.Get(i) + " ");
+            }
+            Console.WriteLine();
+
+            array.Add(6);
+            Console.WriteLine("Массив после добавления 6:");
+            for (int i = 0; i < array.Count(); i++)
+            {
+                Console.Write(array.Get(i) + " ");
+            }
+            Console.WriteLine();
+
+            array.Remove();
+            Console.WriteLine("Массив после удаления последнего элемента:");
+            for (int i = 0; i < array.Count(); i++)
+            {
+                Console.Write(array.Get(i) + " ");
+            }
+            Console.WriteLine();
+
+            array.Insert(2, 10);
+            Console.WriteLine("Массив после вставки 10 в индекс 2:");
+            for (int i = 0; i < array.Count(); i++)
+            {
+                Console.Write(array.Get(i) + " ");
+            }
+            Console.WriteLine();
+
+            array.Delete(3);
+            Console.WriteLine("Массив после удаления элемента по индексу 3:");
+            for (int i = 0; i < array.Count(); i++)
+            {
+                Console.Write(array.Get(i) + " ");
+            }
+            Console.WriteLine();
+
+            array.DeleteValue(2);
+            Console.WriteLine("Массив после удаления элемента со значением 2:");
+            for (int i = 0; i < array.Count(); i++)
+            {
+                Console.Write(array.Get(i) + " ");
+            }
+            Console.WriteLine();
+
+            int[] max = array.Max();
+            Console.WriteLine($"Максимальное значение в массиве {max[1]} с индексом {max[0]}");
+
+            Console.WriteLine($"Массив заполнен?: {array.IsFull()}");
+
+            Console.WriteLine($"Количество элементов в массиве: {array.Count()}");
+        }
+    }
+}
